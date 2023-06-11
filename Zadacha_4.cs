0e162575fad0e1212d132a876dd9cec6c4d9d936
Ogans77 <135201662+Ogans77@users.noreply.github.com>
2023-06-11 18:35:31 +0300

    Максимальное число из трех Оганесян АС

---
+++ Zadacha_4.cs
@@ -0,0 +1,15 @@
+﻿//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.WriteLine("Введите три числа: ");
+int numberA = int.Parse(Console.ReadLine()!);
+int numberB = int.Parse(Console.ReadLine()!);
+int numberC = int.Parse(Console.ReadLine()!);
+int max = numberA;
+
+if (max < numberB)
+    max = numberB;
+if (max < numberC)
+    max = numberC;
+{
+Console.Write ($"Максимальное число: {max}");
+}
