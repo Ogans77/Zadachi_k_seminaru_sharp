447c6da94ea27c670501eda3a288def09b41c10e
Ogans77 <135201662+Ogans77@users.noreply.github.com>
2023-06-22 19:41:02 +0300

    Возведение числа А в степень В (двумя способами)

---
+++ Zadacha_25.cs
@@ -0,0 +1,28 @@
+﻿/*Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16*/
+
+//Методом pow
+
+/*
+Console.Write("Введите число A: ");
+int numA = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число B: ");
+int numB = int.Parse(Console.ReadLine()!);
+int result = (int)Math.Pow(numA, numB);
+Console.WriteLine($"Число А в степени В = {result}");
+*/
+
+//Пробуем циклом )))
+
+Console.Write("Введите число A: ");
+int numA = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число B: ");
+int numB = int.Parse(Console.ReadLine()!);
+int result = 1;
+
+for(int i = 0; i < numB; i++)
+{
+    result = result * numA;
+}
+ Console.WriteLine($"Число {numA} в степени {numB} = {result}");
