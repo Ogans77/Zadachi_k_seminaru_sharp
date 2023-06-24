af4546257541d51f75422e0a0e8ebc567abf11af
Ogans77 <135201662+Ogans77@users.noreply.github.com>
2023-06-24 21:45:49 +0300

    Сумма элементов нечетных индексов массива

---
+++ Zadacha_36.cs
@@ -0,0 +1,30 @@
+﻿/*Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+Найдите сумму элементов, стоящих на нечётных ИНДЕКСАХ.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0*/
+
+int[] array = GetArray(7);
+Console.WriteLine($"[{String.Join(", ", array)}]");
+int summ = 0;
+int el = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (i % 2 != 0)
+    {
+        el = array [i];
+        summ += el;
+    }
+}
+
+Console.WriteLine($"Сумма элементов нечетных индексов массива = {summ}");
+
+int[] GetArray(int size)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(-10, 10);
+    }
+    return result;
+}
