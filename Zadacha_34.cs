142ba2aa61550244bdc6e20b408200172a2615eb
Ogans77 <135201662+Ogans77@users.noreply.github.com>
2023-06-24 17:22:58 +0300

    Количество четных трехзначных в массиве

---
+++ Zadacha_34.cs
@@ -0,0 +1,26 @@
+﻿/*Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2*/
+
+
+int[] array = GetArray(10); //количество выводимых в массиве элементов
+Console.WriteLine($"[{String.Join(", ", array)}]");
+int even = 0;
+
+foreach(int el in array)
+{
+    even += el % 2 == 0 ? 1 : 0;
+}
+
+Console.WriteLine($"Четных чисел в массиве: {even}");
+
+//заполнение массива
+int[] GetArray(int size)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(100, 1000);
+    }
+    return result;
+}
