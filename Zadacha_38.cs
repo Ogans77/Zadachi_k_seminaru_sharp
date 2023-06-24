1f5026a95bb6328ae1f6edd809f35ac64cf9f535
Ogans77 <135201662+Ogans77@users.noreply.github.com>
2023-06-24 19:38:04 +0300

    Разница между максимальным и минимальным в массиве

---
+++ Zadacha_38.cs
@@ -0,0 +1,23 @@
+﻿/*Задача 38: Задайте массив целых двухзначных чисел. 
+Найдите разницу между максимальным и минимальным элементов массива.
+[22, 42, 15, 77, 65] => 77 - 15 = 62*/
+
+int[] array = GetArray(10); //количество выводимых в массиве элементов
+Console.WriteLine($"[{String.Join(", ", array)}]");
+int max = array.Max();
+int min = array.Min();
+
+int dif = max - min;
+
+Console.Write($"Max = {max}, Min = {min}, Разница = {dif}");
+
+//заполнение массива
+int[] GetArray(int size)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(10, 100);
+    }
+    return result;
+}
