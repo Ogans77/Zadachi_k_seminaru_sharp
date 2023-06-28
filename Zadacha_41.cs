173be804f2c65e6ebc88290cb32207b12ecea4e0
Ogans77 <135201662+Ogans77@users.noreply.github.com>
2023-06-28 19:05:16 +0300

    Поиск количества положительных чисел в массиве

---
+++ Zadacha_41.cs
@@ -0,0 +1,27 @@
+﻿/*Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3*/
+
+
+//Задание длины и заполнение массива пользователем
+Console.Write("Задайте количество вводимых чисел: ");
+int size = int.Parse(Console.ReadLine()!);
+
+int[] number = new int[size];
+int i = 0;
+while (i < size)
+{
+    Console.Write("Введите Ваши числа: ");
+    number[i] = int.Parse(Console.ReadLine()!);
+    Console.WriteLine();
+    i++;
+}
+
+//Поиск чисел больше нуля
+int even = 0;
+foreach(int el in number)
+{
+    even += el > 0 ? 1 : 0;
+}
+
+Console.WriteLine($"Положительных чисел (> 0) в массиве: {even}");
