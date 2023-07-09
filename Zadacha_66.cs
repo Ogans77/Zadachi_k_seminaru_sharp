e1a9d53c28af29a23cd8f60518bc486707737e20
Ogans77 <135201662+Ogans77@users.noreply.github.com>
2023-07-09 22:35:20 +0300

    Сумма чисел от M до N рекурсия

---
+++ Zadacha_66.cs
@@ -0,0 +1,25 @@
+﻿/*
+Задача 66: Задайте значения M и N. 
+Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N. 
+Выполнить с помощью рекурсии.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+Console.Write("Введите первое число M: ");
+int M = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число N: ");
+int N = int.Parse(Console.ReadLine()!);
+
+void GetSumNumber(int M, int N, int sum)
+{
+    if (M > N)
+    {
+        Console.WriteLine($"Сумма чисел от M до N = {sum}");
+        return;
+    }
+    sum += (M++);
+    GetSumNumber(M, N, sum);
+}
+
+GetSumNumber(M, N, 0);
